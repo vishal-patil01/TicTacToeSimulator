d2cad2470391c2f01a90f4ac7be96c2e707db665
Vishal Patil <vishalpatil01@hotmail.com>
2020-02-07 17:49:52 +0530

    ADDED : Resetting Game Board For Players

--- TicTacToeSimulator.sh
+++ TicTacToeSimulator.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 echo "Welcome To TicTacToe Simulator "
+
+#!Initializing variable 
+declare -a gameBoard
+
+#!Resetting Game Board By Initilizing Array With Default Value
+function resetBoard() {
+	gameBoard=(1 2 3 4 5 6 7 8 9)
+}
