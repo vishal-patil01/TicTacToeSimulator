f36dbb007240e4503b639cc7beae3e8d12a0924d
Vishal Patil <vishalpatil01@hotmail.com>
2020-02-07 18:54:26 +0530

    ADDED : Printing Game Board And Checking Invalid Cells

--- TicTacToeSimulator.sh
+++ TicTacToeSimulator.sh
@@ -3,10 +3,25 @@ echo "Welcome To TicTacToe Simulator "
 
 #!Initializing variable 
 declare -a gameBoard
+playerMoves=0
+
+#!Initializing Constants
+TOTAL_MOVES=9
 
 #!Resetting Game Board By Initilizing Array With Default Value
 function resetBoard() {
 	gameBoard=(1 2 3 4 5 6 7 8 9)
+	displayBoard
+}
+
+#!Displaying GameBoard
+function displayBoard() {
+	echo "-------------"
+	for((i=0;i<9;i+=3))
+	do
+		echo "| ${gameBoard[$i]} | ${gameBoard[$((i+1))]} | ${gameBoard[$((i+2))]} |"
+		echo "-------------"
+	done
 }
 
 #!Assigning Letter X or O To Player and decide who Play First 
@@ -14,14 +29,46 @@ function tossForPlay() {
 	if [ $((RANDOM % 2)) -eq 0 ]
 	then
 		playerSign='X'
-		playerTurn=true
 	else
 		playerSign='O'
-		playerTurn=true
 	fi
 	echo "Player1 Sign $playerSign"
+}
+
+#!User play Function
+function  userPlay() {
+	read -p "Enter Position Between 1 to 9 : " position
+	if [[ $position -ge 1 && $position -le 9 ]]
+	then
+		positionIsOccupy $position
+	else
+		echo "Invalid Position out Of Board"
+		userPlay
+	fi
+}
+
+#!Run Game Untill Game Ends
+function playTillGameEnd() {
+	while [ $playerMoves -lt $TOTAL_MOVES ]
+	do
+		userPlay
+		displayBoard
+	done
+}
+
+#!checking Position is already filled or blank
+function positionIsOccupy() {
+	local position=$1-1
+	if((${gameBoard[position]}!=$playerSign))
+	then
+		gameBoard[$position]=$playerSign
+		((playerMoves++))
+	else
+		echo "Position is Occupied"
+	fi
 }
 
 #!Starting Game
 resetBoard
 tossForPlay
+playTillGameEnd
