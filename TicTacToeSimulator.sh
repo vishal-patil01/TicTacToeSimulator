b1e2fea09517590ca1ba75dd18b8dbb1a15321db
Vishal Patil <vishalpatil01@hotmail.com>
2020-02-07 18:50:51 +0530

    ADDED : Deciding Who Will Play First

--- TicTacToeSimulator.sh
+++ TicTacToeSimulator.sh
@@ -9,16 +9,19 @@ function resetBoard() {
 	gameBoard=(1 2 3 4 5 6 7 8 9)
 }
 
-#!Assigning Letter X or O To Player 
-function assignSignToPlayer() {
-	if [ $((RANDOM % 2)) -eq 1 ]
+#!Assigning Letter X or O To Player and decide who Play First 
+function tossForPlay() {
+	if [ $((RANDOM % 2)) -eq 0 ]
 	then
 		playerSign='X'
+		playerTurn=true
 	else
 		playerSign='O'
+		playerTurn=true
 	fi
-	echo $playerSign
+	echo "Player1 Sign $playerSign"
 }
 
+#!Starting Game
 resetBoard
-assignSignToPlayer
+tossForPlay
