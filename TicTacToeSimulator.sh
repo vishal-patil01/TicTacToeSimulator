86533e6be2968ab79caa0d3ad4354db38198e260
Vishal Patil <vishalpatil01@hotmail.com>
2020-02-07 19:00:27 +0530

    ADDED : Checking Row/Column/Diagonal For Winning & Tie Conditions

--- TicTacToeSimulator.sh
+++ TicTacToeSimulator.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash 
 echo "Welcome To TicTacToe Simulator "
 
 #!Initializing variable 
@@ -28,11 +28,16 @@ function displayBoard() {
 function tossForPlay() {
 	if [ $((RANDOM % 2)) -eq 0 ]
 	then
-		playerSign='X'
+		playerSign=X
 	else
-		playerSign='O'
+		playerSign=O
 	fi
 	echo "Player1 Sign $playerSign"
+}
+
+#!switching Sign Assign to players
+function switchSign() {
+		[ $playerSign == X ] && playerSign=O || playerSign=X
 }
 
 #!User play Function
@@ -40,31 +45,58 @@ function  userPlay() {
 	read -p "Enter Position Between 1 to 9 : " position
 	if [[ $position -ge 1 && $position -le 9 ]]
 	then
-		positionIsOccupy $position
+		isPositionOccupy $position
 	else
 		echo "Invalid Position out Of Board"
 		userPlay
 	fi
 }
 
-#!Run Game Untill Game Ends
-function playTillGameEnd() {
-	while [ $playerMoves -lt $TOTAL_MOVES ]
-	do
-		userPlay
-		displayBoard
-	done
-}
-
 #!checking Position is already filled or blank
-function positionIsOccupy() {
+function isPositionOccupy() {
 	local position=$1-1
-	if((${gameBoard[position]}!=$playerSign))
+	if((${gameBoard[position]}!=X && ${gameBoard[position]}!=O))
 	then
 		gameBoard[$position]=$playerSign
 		((playerMoves++))
 	else
 		echo "Position is Occupied"
+		userPlay
+	fi
+}
+
+#!Run Game Untill Game Ends And Switching PlayerSign After Winning Check Using Ternary Operator
+function playTillGameEnd() {
+	while [ $playerMoves -lt $TOTAL_MOVES ]
+	do
+		userPlay
+		displayBoard
+		checkWinningCells
+		switchSign
+	done
+	echo "Game Tie...."
+}
+
+#!checking Column,Rows and Diagonals
+function checkWinningCells() {
+	col=0
+	for((row=0;row<9;row+=3))
+	do
+		checkWinner ${gameBoard[$row]} ${gameBoard[$((row+1))]} ${gameBoard[$((row+2))]}
+		checkWinner ${gameBoard[$col]} ${gameBoard[$((col+3))]} ${gameBoard[$((col+6))]}
+		((col+=2))
+	done
+		checkWinner ${gameBoard[0]} ${gameBoard[4]} ${gameBoard[8]}
+		checkWinner ${gameBoard[2]} ${gameBoard[4]} ${gameBoard[6]}
+}
+
+#!checking Winner
+function checkWinner() {
+	local cell1=$1 cell2=$2 cell3=$3
+	if [ $cell1 == $cell2 ] && [ $cell2 == $cell3 ]
+	then
+		echo "Player Win and Have Sign $playerSign"
+		exit
 	fi
 }
 
@@ -72,3 +104,4 @@ function positionIsOccupy() {
 resetBoard
 tossForPlay
 playTillGameEnd
+
